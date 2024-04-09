131db0f5203f58f5e6ec145a2873752247693a8e
CPUIO <>
2024-04-09 19:24:44 +0300

    js

--- js/begin.js
+++ js/begin.js
@@ -1,6 +1,6 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
 };
-if (getRandomNumber(1,30) == 1) {
+if (getRandomNumber(1,31) == 1) {
     window.location.href = 'Error418.html';
 }
